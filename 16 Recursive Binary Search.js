a1e63ee3fa570a8438a9dd867ea72efd5fe19506
Ankur Atri <ankur@shippigo.com>
2023-06-29 09:35:51 +0530

    Recursive Binary Search algo added

--- 16 Recursive Binary Search.js
+++ 16 Recursive Binary Search.js
@@ -1,6 +1,6 @@
-/** 
-
-Binary Search
+/**
+ 
+Recursive Binary Search
 
 Problem - Given a sorted array of 'n' elements and a target element 't', find the index of '4' in the array. 
 Return -1 if the target element is not found.
@@ -20,32 +20,33 @@ If target is less than the middle element, binary search left half of the array.
 
 If target is greater than middle element, binary search right half of the array.
 
-*/
+ */
 
-function binarySearch(arr, target) {
-  let leftIndex = 0;
-  let rightIndex = arr.length - 1;
-
-  while (leftIndex <= rightIndex) {
-    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
-    let middleElement = arr[middleIndex];
-
-    if (target === middleElement) {
-      return middleIndex;
-    }
-
-    if (target < middleElement) {
-      rightIndex = middleIndex - 1;
-    } else {
-      leftIndex = middleIndex + 1;
-    }
-  }
-
-  return -1;
+function recursiveBinarySearch(arr, target) {
+  return search(arr, target, 0, arr.length - 1);
 }
 
-console.log(binarySearch([-5, 2, 4, 6, 10], 10)); // 4
-console.log(binarySearch([-5, 2, 4, 6, 10], 6)); // 3
-console.log(binarySearch([-5, 2, 4, 6, 10], 20)); // -1
+function search(arr, target, leftIndex, rightIndex) {
+  if (leftIndex > rightIndex) {
+    return -1;
+  }
+
+  let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+  let middleElement = arr[middleIndex];
+
+  if (target === middleElement) {
+    return middleIndex;
+  }
+
+  if (target < middleElement) {
+    return search(arr, target, leftIndex, middleIndex - 1);
+  } else {
+    return search(arr, target, middleIndex + 1, rightIndex);
+  }
+}
+
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 10)); // 4
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 6)); // 3
+console.log(recursiveBinarySearch([-5, 2, 4, 6, 10], 20)); // -1
 
 // Big-O = O(log n) Logarithmic - Input size reduced by half
