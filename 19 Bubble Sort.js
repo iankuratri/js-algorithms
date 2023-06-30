14f7ce821ecec2d656bfef82572b2dcd52147e1f
Ankur Atri <ankur@shippigo.com>
2023-06-30 14:24:29 +0530

    Bubble Sort Solution added

--- 19 Bubble Sort.js
+++ 19 Bubble Sort.js
@@ -26,3 +26,29 @@ End of array. Elements swapped? Yes? Repeat the comparison.
 End of array. Elements swapped? No? Array is sorted.
  
 */
+
+function bubbleSort(arr) {
+  let swapped = false;
+
+  do {
+    swapped = false;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+      const currentEl = arr[i];
+      const nextEl = arr[i + 1];
+
+      if (currentEl > nextEl) {
+        arr[i] = nextEl;
+        arr[i + 1] = currentEl;
+
+        swapped = true;
+      }
+    }
+  } while (swapped);
+
+  return arr;
+}
+
+console.log(bubbleSort([-6, 20, 8, -2, 4])); // [ -6 -2 4 8 20 ]
+
+// Big-O = O(n^2) Quadratic Time Complexity -> nested loops
